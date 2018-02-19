901843c2bdc91540a6b0c754ee45882df5e0092d
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-02-19 17:29:37 -0500

    try and catch

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 function yearOfBirth(age) {
-	let yearOfBirth = 2018 - age;
-	return yearOfBirth;
+    try {
+		if(age < 0){
+			throw new Error("Age can not be negative");
+		}
+	  let yearOfBirth = 2018 - age;
+	  return yearOfBirth;	
+	}
+
+	catch (e) {
+   console.log(e);
+	}
+
 }
-
-
 function whoAmI(name, age){
 	let yob = yearOfBirth(age);
 console.log(`Hi, my name is ${name} and I'm ${age} years old`);
 console.log(`I was born in ${yob}`);
 
 }
-whoAmI("Tharaka", 28);
+whoAmI("Tharaka", -28);
